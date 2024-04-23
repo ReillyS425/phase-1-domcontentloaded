ec64de336c096355f9444b2a8759af4041615af3
Reilly Shofner <reilly.shofner@gmail.com>
2024-04-23 14:45:39 -0700

    complete domcontentloaded lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let textElementP = document.getElementById("text");
+   textElementP.textContent = textElementP.textContent.replace("JavaScript is so cool. It lets me add text to my page programmatically.", "This is really cool!")
+});
